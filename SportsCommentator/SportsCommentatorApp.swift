ccaaebba5f86d4bbd5ddeab36a064b9f20f6847f
Zacary Brown <zacarybrown360@gmail.com>
2026-04-07 11:52:54 -0700

    Initial Commit

---
+++ SportsCommentator/SportsCommentatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsCommentatorApp.swift
+//  SportsCommentator
+//
+//  Created by Zacary Brown on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct SportsCommentatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
